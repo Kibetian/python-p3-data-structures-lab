6bf95011c9c3c9f60512dbed3ffb50e9b0644352
Kibetian <126007714+Kibetian@users.noreply.github.com>
2023-08-20 01:16:07 +0300

    done

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -13,26 +13,30 @@ spicy_foods = [
         "name": "Mapo Tofu",
         "cuisine": "Sichuan",
         "heat_level": 6,
-    },
+    }
 ]
 
-def get_names(spicy_foods):
-    pass
+def get_names():
+    return [food["name"] for food in spicy_foods]
 
-def get_spiciest_foods(spicy_foods):
-    pass
+def get_spiciest_foods():
+    return [food for food in spicy_foods if food["heat_level"] > 5]
 
-def print_spicy_foods(spicy_foods):
-    pass
+def print_spicy_foods():
+    for food in spicy_foods:
+        heat_level_emojis = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level_emojis}")
 
-def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+def get_spicy_food_by_cuisine(cuisine):
+    return next((food for food in spicy_foods if food["cuisine"] == cuisine), None)
 
-def print_spiciest_foods(spicy_foods):
-    pass
+def print_spiciest_foods():
+    spiciest_foods = get_spiciest_foods()
+    for food in spiciest_foods:
+        heat_level_emojis = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level_emojis}")
 
-def get_average_heat_level(spicy_foods):
-    pass
-
-def create_spicy_food(spicy_foods, spicy_food):
-    pass
+def average_heat_level():
+    total_heat_level = sum(food["heat_level"] for food in spicy_foods)
+    num_foods = len(spicy_foods)
+    return total_heat_level // num_foods
